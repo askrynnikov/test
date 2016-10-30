8079696ea973cd140a64215a430f0c7cb7d59045
askrynnikov <5101233@gmail.com>
2016-10-30 08:03:52 +0300

    v 0.0.0.2

--- test.rb
+++ test.rb
@@ -7,44 +7,45 @@ results = File.readlines("#{__dir__}/data/results.txt").map(&:chomp)
 
 answers = "1 - да\n2 - нет\n3 - иногда"
 mark = 0
-questions.each do |item|
-  answer = nil
-  loop do
-    puts item
+questions.each do |question|
+  while true
+    puts question
     puts answers
     answer = STDIN.gets.to_i
     break if [1, 2, 3].include?(answer)
     puts "\nОтвет: не удалось распознать"
   end
 
-  case answer
-    when 1 #да
-      mark += 2
-    when 2 #нет
-      mark += 0
-    when 3 #иногда
-      mark += 1
-  end
+  mark +=
+    case answer
+    when 1
+      2 #да
+    when 2
+      0 #нет
+    when 3
+      1 #иногда
+    end
 end
 
 puts "\nРезультаты теста: #{mark} баллов\n"
 answer =
-    case mark
-      when (30..31) then
-        results[0]
-      when (25..29) then
-        results[1]
-      when (19..24) then
-        results[2]
-      when (14..18) then
-        results[3]
-      when (9..13) then
-        results[4]
-      when (4..8) then
-        results[5]
-      when (0..3) then
-        results[6]
-      else
-        results[7]
-    end
+  case mark
+  when (30..31)
+    results[0]
+  when (25..29)
+    results[1]
+  when (19..24)
+    results[2]
+  when (14..18)
+    results[3]
+  when (9..13)
+    results[4]
+  when (4..8)
+    results[5]
+  when (0..3)
+    results[6]
+  else
+    results[7]
+  end
+
 puts proposal_separate_line(answer)
