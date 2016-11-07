62f377f3c2a956cea00a7261847301e47fbd090b
askrynnikov <5101233@gmail.com>
2016-11-07 09:56:23 +0300

    Exhaust notes Vadim from 06.11.16

--- lib/test.rb
+++ lib/test.rb
@@ -3,15 +3,11 @@ class Test
              { name: 'нет', input: '2', score: 0 },
              { name: 'иногда', input: '3', score: 1 }]
 
-  def self.proposal_separate_line(big_string)
-    big_string.gsub(/([\.\?!])( *)/, "\\1\n")
-  end
-
   def initialize
     @questions = File.readlines("#{__dir__}/../data/questions.txt").map(&:chomp)
     @results = File.readlines("#{__dir__}/../data/results.txt").map(&:chomp)
 
-    @mark = 0
+    @scores = 0
     @current_question_number = 0
     @questions_number = @questions.size
   end
@@ -21,9 +17,9 @@ class Test
   end
 
   def result_string
-    if test_passed?
-      "Результаты теста: #{@mark} баллов. " +
-      case @mark
+    if passed?
+      "Результаты теста: #{@scores} баллов. " +
+      case @scores
       when (30..31) then @results[0]
       when (25..29) then @results[1]
       when (19..24) then @results[2]
@@ -36,25 +32,26 @@ class Test
     end
   end
 
-  def ask_question
-    unless test_passed?()
+  def current_question_and_answers
+    unless passed?
       @questions[@current_question_number] + "\n" + answers
     end
   end
 
-  def test_passed?
+  def passed?
     @current_question_number >= @questions_number
   end
 
+  def answer_scoring(answer)
+    @scores += ANSWERS.find { |a| a[:input] == answer }[:score]
+  end
+
+  def next_question
+    @current_question_number += 1
+  end
+
   def answer_admissible?(answer)
-    unless test_passed?()
-      if ANSWERS.any? { |a| a[:input] == answer }
-        @current_question_number += 1
-        @mark += ANSWERS.find { |a| a[:input] == answer }[:score]
-        true
-      else
-        false
-      end
-    end
+    return false if passed?
+    ANSWERS.any? { |a| a[:input] == answer }
   end
 end
--- main.rb
+++ main.rb
@@ -1,15 +1,25 @@
 require_relative 'lib/test'
 
-test = Test.new
-loop do
-  break if test.test_passed?
-  question = test.ask_question
-  loop do
-    puts question
-    answer = STDIN.gets.chomp
-    break if test.answer_admissible?(answer)
-    puts "Ответ: #{answer} не удалось распознать"
-  end
+def separate_line(string)
+  string.gsub(/([\.\?!])( *)/, "\\1\n")
 end
 
-puts Test.proposal_separate_line(test.result_string)
+test = Test.new
+until test.passed?
+  answer = nil
+  loop do
+    puts test.current_question_and_answers
+    answer = STDIN.gets.chomp
+    break if test.answer_admissible?(answer)
+    puts "Ответ: #{answer} не удалось распознать."
+  end
+  test.answer_scoring(answer)
+  test.next_question
+end
+
+puts separate_line(test.result_string)
+
+
+
+
+
